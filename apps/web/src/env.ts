e1d0c354edcb3788d82b81f8d7f9fd4d0b3bc57c
pronoy656 <pronoypaul56@gmail.com>
2026-09-08 11:16:19 +0600

    feat: implement environment variable validation using @t3-oss/env-nextjs and configure turbo global environment variables

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -3,13 +3,16 @@ import { z } from "zod";
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_SITE_URL: z.string().url(),
-    NEXT_PUBLIC_API_URL: z.string().url(),
+    NEXT_PUBLIC_SITE_URL: z.string().url().default("http://localhost:3000"),
+    NEXT_PUBLIC_API_URL: z.string().url().default("http://localhost:5000/api/v1"),
     NEXT_PUBLIC_GA_ID: z.string().optional()
   },
   runtimeEnv: {
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID
-  }
+  },
+  emptyStringAsUndefined: true,
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION
 });
+
